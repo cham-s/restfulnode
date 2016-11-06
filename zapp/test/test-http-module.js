6fa959d5f3a9f5aaa922f0c58998e1b0c93443ad
cham-s <cattouma@student.42.fr>
2016-11-06 16:02:49 +0100

    added chapter3

---
+++ zapp/test/test-http-module.js
@@ -0,0 +1,16 @@
+var sinon = require('sinon');
+exports.test_handle_GET_request = (test) => {
+    var response = { 'writeHead' : function() {},
+                     'end': function() {}
+    };
+    var responseMock = sinon.mock(response);
+    responseMock.expects('end').once().withArgs('Get action was requested');
+    responseMock.expects('writeHead').once().withArgs(200, {'Content-type': 'text/plain'});
+    var request = {};
+    var requestMock = sinon.mock(request);
+    requestMock.method = 'GET';
+    var http_module = require('../modules/http-module');
+    http_module.handle_request(requestMock, response);
+    responseMock.verify();
+    test.done();
+};
